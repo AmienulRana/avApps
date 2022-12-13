5f944ec927992595949abc9a9ce15eab358c61a8
AmienulRana <amienulrana@gmail.com>
2022-12-14 06:56:04 +0700

    membuat layout admin

---
+++ src/components/Layout/Admin.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="flex bg-primary h-auto">
+    <Sidebar />
+    <main class="relative w-full min-h-screen bg-whitesmoke">
+      <slot />
+    </main>
+  </section>
+</template>
+
+<script>
+import Sidebar from "../Sidebar/Sidebar.vue";
+export default {
+  name: "LayoutAdmin",
+  components: { Sidebar },
+};
+</script>
+
+<style scoped>
+</style>
