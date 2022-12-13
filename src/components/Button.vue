4f6ac242d26b681db4a49b5cae67e2d4c88637b8
AmienulRana <amienulrana@gmail.com>
2022-12-14 06:57:55 +0700

    membuat komponen button, agar bisa melakukan styling yang sama pada button

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="flex justify-center items-center shadow-md">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ButtonComponent",
+};
+</script>
+
+<style scoped>
+</style>
