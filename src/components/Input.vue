2397996244368b3f2c14dc510b03918e9a19a2c4
AmienulRana <amienulrana@gmail.com>
2022-12-14 06:58:30 +0700

    membuat komponen input, agar bisa melakukan styling yang sama pada input

---
+++ src/components/Input.vue
@@ -0,0 +1,28 @@
+<template>
+  <input
+    type="text"
+    placeholder="Search"
+    class="
+      w-full
+      border
+      focus:outline focus:outline-primary
+      pr-2
+      py-1.5
+      rounded-full
+    "
+    :class="icon ? 'pl-8' : 'pl-2'"
+    autofocus
+  />
+</template>
+
+<script>
+export default {
+  name: "InputComponent",
+  props: {
+    icon: Boolean,
+  },
+};
+</script>
+
+<style scoped>
+</style>
