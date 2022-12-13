7a3ecb0b946fbfe451a49ca116167db15f7d871e
AmienulRana <amienulrana@gmail.com>
2022-12-14 06:55:27 +0700

    melakukan importing pada package font-awesome, dan membuat konfiguradinya

--- src/main.js
+++ src/main.js
@@ -1,4 +1,43 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/styles/index.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-createApp(App).mount('#app')
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import {
+  faBars,
+  faBriefcase,
+  faCalendar,
+  faChevronUp,
+  faClock,
+  faCreditCard,
+  faExpand,
+  faGear,
+  faMagnifyingGlass,
+  faMoon,
+  faTable,
+  faUsers,
+  faUserTie,
+} from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(
+  faUsers,
+  faUserTie,
+  faChevronUp,
+  faExpand,
+  faMoon,
+  faBars,
+  faTable,
+  faMagnifyingGlass,
+  faClock,
+  faCalendar,
+  faCreditCard,
+  faBriefcase,
+  faGear
+);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
