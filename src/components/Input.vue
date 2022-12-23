3831f8781e076168864e412b88085817d8e44686
AmienulRana <amienulrana@gmail.com>
2022-12-23 23:58:02 +0700

    menambahkan props untuk meng-custom/menambahkan class pada label

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,24 +1,15 @@
 <template>
   <div class="flex justify-between items-center mb-5">
     <label
-      :class="label_class"
-      class="text-sm text-gray-400 w-1/5 min-w-max"
+      :class="label_class ? label_class : 'w-1/5 text-gray-400'"
+      class="text-sm min-w-max"
       v-if="label"
       >{{ label }}</label
     >
     <input
       :type="type"
       :placeholder="placeholder"
-      class="
-        w-full
-        border border-gray-600
-        focus:outline focus:outline-primary
-        pr-2
-        py-1.5
-        form-input form-input:focus:bg-blue-300
-        bg-white
-        rounded
-      "
+      class="w-full border border-gray-600 focus:outline focus:outline-primary pr-2 py-1.5 form-input form-input:focus:bg-blue-300 bg-white rounded"
       :class="[icon ? 'pl-8' : 'pl-2', input_class ? input_class : '']"
       autofocus
     />
@@ -31,6 +22,7 @@ export default {
   props: {
     icon: Boolean,
     label: String,
+    label_class: String,
     input_class: String,
     placeholder: String,
     type: {
@@ -41,5 +33,4 @@ export default {
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
