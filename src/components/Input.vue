aab8520d9554a5de4d1bcf7a7c0fe4199bd064ec
AmienulRana <amienulrana@gmail.com>
2022-12-25 13:16:56 +0700

    menghapus class border-gray-400 pada input

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -9,7 +9,7 @@
     <input
       :type="type"
       :placeholder="placeholder"
-      class="w-full border border-gray-600 focus:outline focus:outline-primary pr-2 py-1.5 form-input form-input:focus:bg-blue-300 bg-white rounded"
+      class="w-full border outline-gray-600 focus:outline focus:outline-primary pr-2 py-1.5 bg-white rounded"
       :class="[icon ? 'pl-8' : 'pl-2', input_class ? input_class : '']"
       autofocus
     />
