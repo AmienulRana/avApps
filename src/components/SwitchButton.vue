9810ee5e84a265b6a204e344aff9614352beaaa3
AmienulRana <amienulrana@gmail.com>
2022-12-25 08:57:33 +0700

    mengganti translate pada indicator, menjadi 7

--- src/components/SwitchButton.vue
+++ src/components/SwitchButton.vue
@@ -1,22 +1,13 @@
 <template>
   <div
-    class="
-      w-14
-      h-6
-      flex
-      items-center
-      rounded-full
-      p-1
-      duration-300
-      cursor-pointer
-    "
+    class="w-14 h-6 flex items-center rounded-full p-1 duration-300 cursor-pointer"
     :class="value ? 'bg-primary' : 'bg-gray-300'"
     :aria-checked="value.toString()"
     @click="toggle"
   >
     <div
       class="bg-white w-4 h-4 rounded-full shadow-md transform duration-300"
-      :class="{ 'translate-x-8': value }"
+      :class="{ 'translate-x-7': value }"
     ></div>
   </div>
 </template>
