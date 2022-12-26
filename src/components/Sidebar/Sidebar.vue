968a8dc23869f81b4973bd27a9eebf531be265ab
AmienulRana <amienulrana@gmail.com>
2022-12-26 21:03:44 +0700

    mengirimkan props modeSidebar untuk membuat kondisi pada tampilan sidebar, jika modeSidebar === 'icon' maka akan  menghilangkan text pada sidebar jika, dan hanya menampilkan icon nya saja

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,31 +1,62 @@
 <template>
-  <section class="min-h-screen bg-primary w-64 px-5 py-5 hidden md:block">
-    <nav class="text-center">
-      <h1 class="text-2xl text-white">Mufidah Group</h1>
+  <section
+    class="min-h-screen bg-primary px-5 py-5 hidden md:block duration-300"
+    :class="modeSidebar === 'icon' ? 'w-20' : 'w-64'"
+  >
+    <nav class="text-center m-auto">
+      <h1 class="text-2xl text-white text-center duration-300 h-8">
+        M<span :class="modeSidebar === 'icon' ? 'scale-0 hidden' : ''"
+          >ufidah Group</span
+        >
+      </h1>
       <span class="w-full h-px bg-white mt-5 opacity-80 block"></span>
 
       <section class="flex items-center text-white text-md my-4">
-        <div class="w-10 h-9 flex items-center justify-center bg-blue-500 mr-3">
+        <div
+          class="bg-icon flex items-center justify-center bg-blue-500"
+          :class="{ 'mr-3': modeSidebar !== 'icon' }"
+        >
           <font-awesome-icon icon="fa-home-alt" class="text-lg" />
         </div>
         <router-link to="/">
-          <p class="text-sm">Dashboard</p>
+          <p
+            class="text-sm duration-300"
+            :class="modeSidebar === 'icon' ? 'hidden' : ''"
+          >
+            Dashboard
+          </p>
         </router-link>
       </section>
       <template v-for="(menu, i) in menuSidebar" :key="i">
         <Accordion
-          class="text-white text-md my-4"
+          class="text-white text-md my-4 m-auto"
           @click:header="handleToggleAccordion(menu.title)"
           :isOpen="isOpen === menu.title"
+          :icon="false"
         >
           <template v-slot:header>
-            <section class="flex items-center">
-              <div
-                class="w-10 h-9 flex items-center justify-center bg-blue-500 mr-3"
-              >
-                <font-awesome-icon :icon="menu.icon" class="text-lg" />
+            <section class="flex items-center justify-between w-full">
+              <div class="flex items-center">
+                <div
+                  class="w-10 h-9 flex items-center justify-center bg-blue-500 mr-3"
+                >
+                  <font-awesome-icon :icon="menu.icon" class="text-lg" />
+                </div>
+                <p
+                  class="text-sm duration-300"
+                  :class="modeSidebar === 'icon' ? 'hidden' : ''"
+                >
+                  {{ menu.title }}
+                </p>
               </div>
-              <p class="text-sm">{{ menu.title }}</p>
+              <font-awesome-icon
+                icon="fa-chevron-up w-6 h-6"
+                class="duration-300 ease-in-out"
+                :class="[
+                  isOpen !== menu.title ? '' : 'rotate-180',
+                  { 'scale-0': modeSidebar === 'icon' },
+                ]"
+              />
             </section>
           </template>
           <template v-slot:content>
@@ -133,5 +164,9 @@ p.text-white:hover > span {
 p > span {
   min-width: 5px;
   min-height: 5px;
+}
+.bg-icon {
+  min-width: 40px;
+  min-height: 36px;
 }
 </style>
