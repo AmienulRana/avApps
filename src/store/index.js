f1a8bb28789ea658d71228c6726bc5b10cec56b4
AmienulRana <amienulrana@gmail.com>
2022-12-28 15:24:45 +0700

    membuat folder store untuk menyimpan semua yang bersangkutan sama vuex agar lebih rapi

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  state() {
+    return {
+      sidebarActive: "",
+    };
+  },
+  mutations: {},
+  actions: {},
+});
+
+export default store;
