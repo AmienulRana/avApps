ce3199deb90c299a9e8162cc90ec4337df6199af
AmienulRana <amienulrana@gmail.com>
2023-01-01 21:23:12 +0700

    membuat file action/employment untuk meng-handle semua request ke server khusu employment

---
+++ src/actions/employment.js
@@ -0,0 +1,32 @@
+import { URL_API } from "@/config";
+import { tokenDecrypted } from "@/utils/decryptToken";
+import axios from "axios";
+
+export const AddEmploymentAPI = async (payload, query) => {
+  try {
+    const response = await axios.post(
+      `${URL_API}/employment${query}`,
+      payload,
+      {
+        headers: {
+          Authorization: `Bearer ${tokenDecrypted()}`,
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+export const GetAllEmployementAPI = async (query) => {
+  try {
+    const response = await axios.get(`${URL_API}/employment${query}`, {
+      headers: {
+        Authorization: `Bearer ${tokenDecrypted()}`,
+      },
+    });
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
