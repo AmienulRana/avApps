5ded1bb7617cfba9567e4e05780e378cf31d4f17
AmienulRana <amienulrana@gmail.com>
2023-01-09 19:30:47 +0700

    menambahkan kondisi jika token di localStorage masih null, maka pakai token yang ada di global state vuex

--- src/utils/decryptToken.js
+++ src/utils/decryptToken.js
@@ -1,9 +1,10 @@
 import { SECRET_KEY } from "@/config";
 import CryptoJS from "crypto-js";
 import jwtDecode from "jwt-decode";
+import store from "../store";
 
 function decryptToken() {
-  const encryptedToken = localStorage.getItem("token");
+  const encryptedToken = store.state.token || localStorage.getItem("token");
   const bytes = CryptoJS.AES.decrypt(encryptedToken, SECRET_KEY);
   const token = bytes.toString(CryptoJS.enc.Utf8);
   const payload = jwtDecode(token);
@@ -13,7 +14,7 @@ function decryptToken() {
 export default decryptToken;
 
 export function tokenDecrypted() {
-  const encryptedToken = localStorage.getItem("token");
+  const encryptedToken = store.state.token || localStorage.getItem("token");
   const bytes = CryptoJS.AES.decrypt(encryptedToken, SECRET_KEY);
   return bytes.toString(CryptoJS.enc.Utf8);
 }
