8d24db91519967a555200b63dd526f41ace50a04
AmienulRana <amienulrana@gmail.com>
2023-01-14 12:46:35 +0700

    menghapus tanda, koma dalam pengembalian tanggal

--- src/utils/getDate.js
+++ src/utils/getDate.js
@@ -14,6 +14,6 @@ export function getDate() {
     "Nov",
     "Dec",
   ];
-  let date = d.getDate() + ", " + months[d.getMonth()] + " " + d.getFullYear();
+  let date = d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear();
   return date;
 }
