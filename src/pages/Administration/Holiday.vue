8ba2f5d69022d6187560a11b23eb9c8e29b257fc
AmienulRana <amienulrana@gmail.com>
2023-01-18 12:50:23 +0700

    .

--- src/pages/Administration/Holiday.vue
+++ src/pages/Administration/Holiday.vue
@@ -19,15 +19,15 @@
       </section>
       <section class="bg-white mt-10 py-5 px-10 mb-10">
         <section class="mb-2">
-          <h1 class="text-xl ml-5">
+          <h1 class="text-xl text-gray-400 ml-5">
             {{ periodic?.periodic_month }} {{ periodic?.periodic_years }}
           </h1>
-          <p class="ml-5 text-sm text-gray-400">
-            {{ periodic?.periodic_start_date }} -
+          <p class="text-sm text-gray-400 ml-5">
+            {{ periodic?.periodic_start_date }} to
             {{ periodic?.periodic_end_date }}
           </p>
         </section>
-        <section class="w-full mt-10">
+        <section class="w-full mt-10 border">
           <div class="grid grid-cols-7 thead">
             <p
               v-for="day in days"
@@ -43,12 +43,18 @@
                 v-for="day in week"
                 :key="day"
                 :class="[
-                  'h-28 w-full td text-right text-sm',
+                  'h-28 w-full td text-right text-sm not-cuti relative',
                   isToday(day) ? 'bg-blue-500 text-white' : 'text-gray-400',
                   isSunday(day) ? 'text-red-500' : '',
                 ]"
               >
-                {{ day.date }}
+                {{ day?.date }}
+                <br />
+                <span
+                  v-if="day?.isHoliday"
+                  class="text-white rounded-md text-xs bg-primary absolute top-8 right-0 w-full border-r-primary p-1"
+                  >{{ day?.holidayName }}</span
+                >
               </p>
             </section>
           </div>
@@ -65,6 +71,7 @@ import { GetAllCompanyAPI } from "@/actions/company";
 import decryptToken from "@/utils/decryptToken";
 import ChoiseCompany from "@/components/ChoiseCompany.vue";
 import { GetPeriodicActiveAPI } from "@/actions/periodic";
+import { GetLeaveHolidayAPI } from "@/actions/leave-holiday";
 
 export default {
   name: "HolidayPage",
@@ -81,42 +88,21 @@ export default {
         "Sunday",
       ],
       weeksInPeriod: [],
+      holidays: [],
       options: [],
+      periodic: {},
       superAdmin: false,
       dataCompany: {},
-      periodic: {},
       loading: {
         employement: true,
       },
     };
   },
   mounted() {
-    let startDate = moment(this.periodic?.periodic_start_date || "2022-12-25");
-    let endDate = moment(this.periodic?.periodic_end_date || "2023-01-25");
-    let currentWeek = [];
-    let day = startDate.day();
-    for (
-      let m = moment(startDate);
-      m.isSameOrBefore(endDate);
-      m.add(1, "days")
-    ) {
-      currentWeek[day] = { date: m.format("DD"), day: m.format("dddd") };
-      if (
-        day === 6 ||
-        m.isSame(endDate, "day") ||
-        m.date() === moment(m).endOf("month").date()
-      ) {
-        this.weeksInPeriod.push(currentWeek);
-        currentWeek = [];
-        day = 0;
-      } else {
-        day++;
-      }
-    }
-
+    this.getAllCompany();
+    // this.handleCalender();
     const payload = decryptToken();
     this.superAdmin = payload?.role === "Super Admin";
-    this.getAllCompany();
   },
   watch: {
     dataCompany: {
@@ -127,13 +113,61 @@ export default {
     },
   },
   methods: {
+    handleCalender(periodic_start_date, periodic_end_date) {
+      let startDate = moment(periodic_start_date);
+      let endDate = moment(periodic_end_date);
+
+      let currentWeek = [];
+      let day = startDate.day();
+      for (
+        let m = moment(startDate);
+        m.isSameOrBefore(endDate);
+        m.add(1, "days")
+      ) {
+        currentWeek[day] = {
+          date: m.format("DD"),
+          month: m.format("MM"),
+          years: m.format("YYYY"),
+          day: m.format("dddd"),
+          isHoliday: false,
+          holidayName: "",
+        };
+        const holiday = this.checkHoliday(m.format("YYYY-MM-DD"));
+        currentWeek[day].isHoliday = holiday?.isHoliday;
+        currentWeek[day].holidayName = holiday?.holidayName;
+
+        if (
+          day === 6 ||
+          m.isSame(endDate, "day") ||
+          m.date() === moment(m).endOf("month").date()
+        ) {
+          this.weeksInPeriod.push(currentWeek);
+          currentWeek = [];
+          day = 0;
+        } else {
+          day++;
+        }
+      }
+    },
+    checkHoliday(date) {
+      const holiday = this.holidays.find((holiday) => {
+        moment(holiday?.leavehol_startdate).isSameOrBefore(date) &&
+          moment(holiday?.leavehol_enddate).isSameOrAfter(date);
+      });
+      if (holiday) {
+        return { isHoliday: true, holidayName: holiday.leavehol_desc };
+      } else {
+        return { isHoliday: false, holidayName: "" };
+      }
+      // return { isHoliday: false, holidayName: "" };
+    },
     isToday(day) {
-      let date = moment(day.date, "DD");
+      const newDate = `${day?.date}-${day?.month}-${day?.years}`;
+      let date = moment(newDate, "DD-MM-YYYY");
       return moment().isSame(date, "day");
     },
     isSunday(day) {
-      // let date = new Date(day.year, day.month, day.date);
-      return day.day === "Sunday";
+      return day?.day === "Sunday";
     },
     async getAllCompany() {
       const response = await GetAllCompanyAPI();
@@ -144,7 +178,35 @@ export default {
     async getPeriodic() {
       const querySuperAdmin = `?company_id=${this?.dataCompany?._id}`;
       const response = await GetPeriodicActiveAPI(querySuperAdmin);
+      if (response.status === 401) {
+        this.$router.push("/login");
+        this.$store.commit("changeIsLoggedIn", false);
+      }
       this.periodic = response.data;
+      this.getLeaveHoliday();
+
+      this.handleCalender(
+        response.data?.periodic_start_date,
+        response.data?.periodic_end_date
+      );
+    },
+    async getLeaveHoliday() {
+      const queryAdminSuper = `?company_id=${this.dataCompany?._id}`;
+      const response = await GetLeaveHolidayAPI(queryAdminSuper);
+
+      if (response?.status === 401) {
+        this.$router.push("/login");
+        this.$store.commit("changeIsLoggedIn", false);
+      }
+      const selectPropertyHoliday = response?.data.map((holiday) => ({
+        leavehol_desc: holiday?.leavehol_desc,
+        leavehol_enddate: holiday?.leavehol_enddate,
+        leavehol_startdate: holiday?.leavehol_startdate,
+        leavehol_status: holiday?.leavehol_status,
+        leavehol_type: holiday?.leavehol_type,
+      }));
+      this.holidays = selectPropertyHoliday;
+      this.loading.getLeaveHol = false;
     },
   },
 };
@@ -156,6 +218,8 @@ export default {
   padding-top: 5px !important;
   padding-right: 10px !important;
   font-size: 12px;
+}
+.tbody .not-cuti {
   border: 1px solid rgb(239, 239, 239);
 }
 .thead .th {
