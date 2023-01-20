3db12d8b83123ba3ec39fa12b046b3f469a1f2e0
AmienulRana <amienulrana@gmail.com>
2023-01-20 09:08:46 +0700

    menambahkan section baru untuk membungkus loading, agar tidak bisa scroll kesamping

--- src/components/TableAttedance.vue
+++ src/components/TableAttedance.vue
@@ -1,94 +1,96 @@
 <template>
-  <section
-    class="w-full overflow-x-auto custom-scrollbar relative"
-    :class="loading ? 'h-96' : ''"
-  >
-    <table class="bg-white min-w-max mt-6 w-full pb-4">
-      <thead class="border-b bg-white border-gray-200 text-gray-400">
-        <tr>
-          <th class="text-left text-sm">Profile</th>
-          <th class="text-left text-sm">Clock In</th>
-          <th class="text-left text-sm">Clock out</th>
-          <th class="text-left text-sm">Behaviour</th>
-          <th class="text-left text-sm">Type</th>
-          <th class="text-left text-sm">Total Hours</th>
-          <th class="text-left text-sm">Break</th>
-          <th class="text-left text-sm">Orthers Break</th>
-          <th class="text-left text-sm">Deducation</th>
-          <th class="text-left text-sm">Actions</th>
-        </tr>
-      </thead>
-      <tbody v-if="!loading">
-        <tr class="border-b" v-for="(attedance, i) in attendances" :key="i">
-          <td class="flex items-center p-3 text-sm">
-            <div
-              class="w-12 h-12 flex justify-center items-center rounded-full bg-zinc-400"
-            >
-              <h2 class="text-md text-white">IA</h2>
-            </div>
-            <div class="ml-5">
-              <h1 class="text-blue-400 text-base">
-                {{ attedance?.emp_id?.emp_fullname }}
-              </h1>
-              <p class="text-sm mb-1 text-gray-600">
-                {{ attedance?.emp_id?.emp_depid?.dep_name }}
+  <section class="relative">
+    <section
+      class="w-full overflow-x-auto custom-scrollbar relative"
+      :class="loading ? 'h-96' : ''"
+    >
+      <table class="bg-white min-w-max mt-6 w-full pb-4">
+        <thead class="border-b bg-white border-gray-200 text-gray-400">
+          <tr>
+            <th class="text-left text-sm">Profile</th>
+            <th class="text-left text-sm">Clock In</th>
+            <th class="text-left text-sm">Clock out</th>
+            <th class="text-left text-sm">Behaviour</th>
+            <th class="text-left text-sm">Type</th>
+            <th class="text-left text-sm">Total Hours</th>
+            <th class="text-left text-sm">Break</th>
+            <th class="text-left text-sm">Orthers Break</th>
+            <th class="text-left text-sm">Deducation</th>
+            <th class="text-left text-sm">Actions</th>
+          </tr>
+        </thead>
+        <tbody v-if="!loading">
+          <tr class="border-b" v-for="(attedance, i) in attendances" :key="i">
+            <td class="flex items-center p-3 text-sm">
+              <div
+                class="w-12 h-12 flex justify-center items-center rounded-full bg-zinc-400"
+              >
+                <h2 class="text-md text-white">IA</h2>
+              </div>
+              <div class="ml-5">
+                <h1 class="text-blue-400 text-base">
+                  {{ attedance?.emp_id?.emp_fullname }}
+                </h1>
+                <p class="text-sm mb-1 text-gray-600">
+                  {{ attedance?.emp_id?.emp_depid?.dep_name }}
+                </p>
+                <p class="text-xs text-gray-400">
+                  {{ attedance?.shift_id?.shift_name }} ({{
+                    attedance?.workhours_in
+                  }}
+                  - {{ attedance?.workhours_out }})
+                </p>
+              </div>
+            </td>
+            <td class="p-3 text-sm">
+              <p class="text-sm text-gray-400">{{ attedance?.clock_in }}</p>
+            </td>
+            <td class="p-3 text-sm">
+              <p class="text-sm text-gray-400">
+                {{ attedance?.clock_out }}
               </p>
-              <p class="text-xs text-gray-400">
-                {{ attedance?.shift_id?.shift_name }} ({{
-                  attedance?.workhours_in
-                }}
-                - {{ attedance?.workhours_out }})
+            </td>
+            <td class="p-3 text-sm">
+              <p
+                class="flex py-1 text-white w-24 items-center justify-center rounded-full"
+                :class="backgroundBehavior(attedance?.behavior_at)"
+              >
+                {{ attedance?.behavior_at }}
               </p>
-            </div>
-          </td>
-          <td class="p-3 text-sm">
-            <p class="text-sm text-gray-400">{{ attedance?.clock_in }}</p>
-          </td>
-          <td class="p-3 text-sm">
-            <p class="text-sm text-gray-400">
-              {{ attedance?.clock_out }}
-            </p>
-          </td>
-          <td class="p-3 text-sm">
-            <p
-              class="flex py-1 text-white w-24 items-center justify-center rounded-full"
-              :class="backgroundBehavior(attedance?.behavior_at)"
-            >
-              {{ attedance?.behavior_at }}
-            </p>
-          </td>
-          <td class="p-3 text-sm">
-            <p class="text-sm text-gray-400">{{ attedance?.type }}</p>
-          </td>
-          <td class="p-3 text-sm">
-            <p class="text-sm text-gray-400">{{ attedance?.workhours }}</p>
-          </td>
-          <td class="p-3 text-sm">
-            <p class="text-sm text-gray-400">
-              Break in : {{ attedance?.break_in }}
-            </p>
-            <p class="text-sm text-gray-400">
-              Break out : {{ attedance?.break_out }}
-            </p>
-          </td>
-          <td class="p-3 text-sm">
-            <p class="text-sm text-gray-400 underline">View orthers Break</p>
-          </td>
-          <td class="p-3 text-sm">
-            <p class="text-sm text-gray-400 underline">View Potongan</p>
-          </td>
-          <td class="p-3 text-right relative">
-            <Button
-              class="p-3 shadow-none rotate-90 hover:bg-blue-100 text-primary rounded-full"
-            >
-              <font-awesome-icon icon="fa-ellipsis" />
-            </Button>
-          </td>
-        </tr>
-      </tbody>
-      <Loading v-if="loading" />
-      <NoDataShowing v-if="!loading && attendances.length === 0" />
-    </table>
+            </td>
+            <td class="p-3 text-sm">
+              <p class="text-sm text-gray-400">{{ attedance?.type }}</p>
+            </td>
+            <td class="p-3 text-sm">
+              <p class="text-sm text-gray-400">{{ attedance?.workhours }}</p>
+            </td>
+            <td class="p-3 text-sm">
+              <p class="text-sm text-gray-400">
+                Break in : {{ attedance?.break_in }}
+              </p>
+              <p class="text-sm text-gray-400">
+                Break out : {{ attedance?.break_out }}
+              </p>
+            </td>
+            <td class="p-3 text-sm">
+              <p class="text-sm text-gray-400 underline">View orthers Break</p>
+            </td>
+            <td class="p-3 text-sm">
+              <p class="text-sm text-gray-400 underline">View Potongan</p>
+            </td>
+            <td class="p-3 text-right relative">
+              <Button
+                class="p-3 shadow-none rotate-90 hover:bg-blue-100 text-primary rounded-full"
+              >
+                <font-awesome-icon icon="fa-ellipsis" />
+              </Button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+    <Loading v-if="loading" />
+    <NoDataShowing v-if="!loading && attendances.length === 0" />
   </section>
 </template>
 
