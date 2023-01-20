4c2cf127c6bfa6873a22db310c4ec2ff24222808
AmienulRana <amienulrana@gmail.com>
2023-01-20 09:04:17 +0700

    menghapus credential

--- src/actions/company.js
+++ src/actions/company.js
@@ -8,7 +8,6 @@ export const GetAllCompanyAPI = async () => {
       headers: {
         Authorization: `Bearer ${tokenDecrypted()}`,
       },
-      withCredentials: true,
     });
     return response;
   } catch (error) {
@@ -21,7 +20,6 @@ export const GetDahsboardAPI = async (query) => {
       headers: {
         Authorization: `Bearer ${tokenDecrypted()}`,
       },
-      withCredentials: true,
     });
     return response;
   } catch (error) {
