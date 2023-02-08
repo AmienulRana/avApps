6a297f99fe98ce7fbe66489ba4841072bb8dbffd
AmienulRana <amienulrana@gmail.com>
2023-02-08 18:43:34 +0700

    menambahkan function untuk melakukan request ke method delete dan put pada url /leave-holiday

--- src/actions/leave-holiday.js
+++ src/actions/leave-holiday.js
@@ -18,9 +18,37 @@ export const AddLeaveHolidayAPI = async (query, payload) => {
     return error.response;
   }
 };
+export const EditLeaveHolidayAPI = async (id, payload) => {
+  try {
+    const response = await axios.put(
+      `${URL_API}/leave-holiday/${id}`,
+      payload,
+      {
+        headers: {
+          Authorization: `Bearer ${tokenDecrypted()}`,
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
 export const GetLeaveHolidayAPI = async (query) => {
   try {
     const response = await axios.get(`${URL_API}/leave-holiday${query}`, {
+      headers: {
+        Authorization: `Bearer ${tokenDecrypted()}`,
+      },
+    });
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+export const DeleteLeaveHolidayAPI = async (id) => {
+  try {
+    const response = await axios.delete(`${URL_API}/leave-holiday/${id}`, {
       headers: {
         Authorization: `Bearer ${tokenDecrypted()}`,
       },
