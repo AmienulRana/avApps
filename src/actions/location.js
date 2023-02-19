e07ba68a43d9002f419781fd193c0c004f4622b1
AmienulRana <amienulrana@gmail.com>
2023-02-19 22:06:06 +0700

    menambahkan function edit dan delete untuk meng-integrasikan dengan api

--- src/actions/location.js
+++ src/actions/location.js
@@ -14,3 +14,39 @@ export const GetLocationAPI = async (query) => {
     return error.response;
   }
 };
+export const DeleteLocationAPI = async (id) => {
+  try {
+    const response = await axios.delete(`${URL_API}/location/${id}`, {
+      headers: {
+        Authorization: `Bearer ${tokenDecrypted()}`,
+      },
+    });
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+export const EditLocationAPI = async (id, payload) => {
+  try {
+    const response = await axios.put(`${URL_API}/location/${id}`, payload, {
+      headers: {
+        Authorization: `Bearer ${tokenDecrypted()}`,
+      },
+    });
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+export const AddLocationAPI = async (payload, query) => {
+  try {
+    const response = await axios.post(`${URL_API}/location${query}`, payload, {
+      headers: {
+        Authorization: `Bearer ${tokenDecrypted()}`,
+      },
+    });
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
